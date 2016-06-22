80278b84acd36b46bc7834726daf8decc10d43fd
Adam S <adam.stepien1@gmail.com>
2016-06-22 19:16:46 +0200

    opisano wszystkie typy poza string

--- src/paczka/dataTypes.java
+++ src/paczka/dataTypes.java
@@ -10,21 +10,39 @@ public class dataTypes {
 	    String[][] data = {
 	            { "byte", "1" },
 	            { "short", "2" },
-	            { "byte", "1" },
+	            { "int", "4" },
+	            { "long", "8" },
+	            { "=", "=" },
+	            { "float", "4" }, //8 liczb po przecinku
+	            { "double", "8" },//16 liczb po przecinku
 	         };
 	    
 	    ASCIITable.getInstance().printTable(header, data);
 		
+	    //typy wraz ze swoimi zakresami
 		byte byteVarMin, byteVarMax;
 			byteVarMin=-128;
 			byteVarMax=127;
-		short shortVar=1;
-		int intVar=1;
-		long longVar=1;
-		System.out.println("byte - 1 bajt pamięci"
-				+ "\nshort - 2 bajty pamięci "
-				+ "\nint - 4 bajty pamięci"
-				+ "\nlong - 8 bajtów pamięci");
+		short shortVarMin, shortVarMax;
+			shortVarMin=-32_768;  //mozna podkreslic dla czytelnosci
+			shortVarMax=32_767;
+		int intVarMin, IntVarMax=1;
+			intVarMin=-2^31;
+			IntVarMax=2^31-1;
+		long longVarMin, longVarMax;
+			longVarMin=-2^63l;		//na koncu longa
+			longVarMax=2^63-1l;		//warto dodac 'l'
+			
+		float flpoint=0.1f;			//do floata dodac f
+		double dblpoint = 0.123d;	//do dbl dodac d
+		
+		char a='a';		//UTF-16
+		char b='\\';	//tak zapiszemy backslash
+		char c='\'';	//a tak apostrof
+		
+		boolean truth=true;	//typ boolowski
+		
+		
 	}
 
 }
